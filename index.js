87f05941deddf41aa7b749f94c8d54bc8803e53f
KNIGHTW4LKER <samyakaditya0000@gmail.com>
2023-08-04 00:46:13 +0530

    second commit

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv'
 import postRoutes from './routes/posts.js'
 
 const app = express();
+dotenv.config();
 
 
 app.use(bodyParser.json({limit : '30mb',extended: true}));
@@ -14,10 +16,10 @@ app.use(cors());
 app.use('/posts',postRoutes);
 
 
-const CONNECTION_URL = 'mongodb+srv://Samyak:Cyber10@cluster0.t5hemdi.mongodb.net/?retryWrites=true&w=majority'
-const PORT = process.env.PORT || 5000
+ 
+const PORT = process.env.PORT
 
-mongoose.connect(CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology: true})
 .then(()=> app.listen(PORT,()=>console.log(`listening on port: ${PORT}`)))
 .catch((error)=> console.log(error.message));
 
